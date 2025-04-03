705d65ebeab9152a984f594c7af6407f67391a55
Kojirou <kojirouhtc@gmail.com>
2025-04-04 03:13:00 +0800

    Update build_plugin.sh
    change l-smash rpeo

--- VapourSynth/build_plugin.sh
+++ VapourSynth/build_plugin.sh
@@ -5,18 +5,18 @@ deps_dir=$PWD/deps
 set -e
 
 git clone https://github.com/HomeOfAviSynthPlusEvolution/FFmpeg -b custom-patches-for-lsmashsource --depth 1
-git clone https://github.com/l-smash/l-smash.git --depth 1
+git clone https://github.com/vimeo/l-smash.git --depth 1
 
 cd l-smash
 ./configure --prefix=$deps_dir --extra-cflags=-fPIC
-make -j
+make -j$(nproc)
 make install
 cd ..
 
 
 cd FFmpeg
 ./configure --prefix=$deps_dir --enable-gpl --enable-version3 --enable-nonfree --enable-static --disable-shared --enable-pic --disable-autodetect --disable-doc --disable-programs
-make -j
+make -j$(nproc)
 make install
 cd ..
 
